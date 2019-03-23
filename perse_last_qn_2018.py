1aa029a5e5845ffb8120da0dcc9049a237faf96b
John <johnkellyoxford@gmail.com>
2019-03-23 09:16:30 +0000

    Add files via upload

--- perse_last_qn_2018.py
+++ perse_last_qn_2018.py
@@ -7,15 +7,18 @@ beacons = [[True, (0, 0)]]
 
 for i in range(num):
     x, y = input().split(" ")
+    # because it is a list of lists, we need to deepcopy not copy later !!
     beacons.append([False, (int(x), int(y))])
 
 prev = -1
 
 while True:
+    # copy so we don't light beacons that were lit this round
     tmp = copy.deepcopy(beacons)
     for i, b in enumerate(beacons):
         for oi, ob in enumerate(beacons):
             dist = math.hypot(b[1][0] - ob[1][0], b[1][1] - ob[1][1])
+            # if one of them is true and they are in distance, both are true
             if b[0] != ob[0] and dist <= ran:                
                 tmp[i][0] = True
                 tmp[oi][0] = True
@@ -27,6 +30,7 @@ while True:
         if b[0] and b != [True, (0, 0)]:
             c += 1
 
+    # if no new beacons lit, break
     if prev == c:
         break
             
