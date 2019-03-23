c7a63fa4a0d155b2732311796b2489680a6b863e
John <johnkellyoxford@gmail.com>
2019-03-23 09:04:50 +0000

    Add files via upload

---
+++ perse_last_qn_2018.py
@@ -0,0 +1,35 @@
+import math
+import copy
+
+ran = int(input())
+num = int(input())
+beacons = [[True, (0, 0)]]
+
+for i in range(num):
+    x, y = input().split(" ")
+    beacons.append([False, (int(x), int(y))])
+
+prev = -1
+
+while True:
+    tmp = copy.deepcopy(beacons)
+    for i, b in enumerate(beacons):
+        for oi, ob in enumerate(beacons):
+            dist = math.hypot(b[1][0] - ob[1][0], b[1][1] - ob[1][1])
+            if b[0] != ob[0] and dist <= ran:                
+                tmp[i][0] = True
+                tmp[oi][0] = True
+                break
+
+            
+    c = 0
+    for b in tmp:
+        if b[0] and b != [True, (0, 0)]:
+            c += 1
+
+    if prev == c:
+        break
+            
+    prev = c
+    beacons = tmp
+    print(c)
