64348388d260ca1d71fbe9d996b8179be85456e1
John <johnkellyoxford@gmail.com>
2019-07-21 15:13:39 +0100

    Add files via upload

---
+++ Int32ToBool.asm
@@ -0,0 +1,43 @@
+; aim - with a 32-bit integer type, turn zero into zero (false) and non-zero into one (true)
+
+; idea one
+one PROC ; ecx:int32 -> eax:int32
+    xor eax, eax ; <--- these 2 can be executed simultaneously - on intel, the xor is eliminated at rename, but apparently not on AMD
+    test ecx, ecx ; <-/
+    setnz al ; <-- this has a dependency on both the 'xor' and the 'test'
+    ret
+one ENDP
+
+; these next 3 are the basic same pattern of test setcc movzx, but differ in what register they setcc before zero extending
+; the idea is that movzx eax, al isn't ideal because supposedly on Haswell + Skylake a movzx <reg> <reg> can be eliminated by the renamer,
+; but not when both registers are part of the same reg (eax and al are subsections of rax). So instead, we setcc and then extend it to eax,
+; so this should be eliminated during rename - this does however mean we now have a dependency on test to finish (as it uses ecx), but I don't think
+; it should be an issue because we already had the dependency on the flags set by test. If it is an issue, idea 4 instead uses the unused dl register for
+; setcc
+
+; all the 'setnz <l8>' ops have a dependency on the 'test'
+
+; idea two
+two PROC ; ecx:int32 -> eax:int32
+    test ecx, ecx 
+    setnz al ; this has a dependency on the flags from 'test'
+    movzx eax, al ; <-- this has a dependency  on 'setcc' which in turn has the dependency on 'test' - however, apparently on 
+                  ; *some* (not all) newer chips it can be rename eliminated - but maybe not when it has that dependency?
+    ret
+two ENDP
+
+; idea three
+three PROC ; ecx:int32 -> eax:int32
+    test ecx, ecx
+    setnz cl ; this has a dependency on the flags AND register from 'test', because it writes to eax/cl
+    movzx eax, cl 
+    ret
+three ENDP
+
+; idea four
+four PROC ; ecx:int32 -> eax:int32
+    test ecx, ecx
+    setnz dl ; this has a dependency on the flags from 'test' but not the register (like two:) but it still has the different registers for the 'movzx'
+    movzx eax, dl
+    ret
+four ENDP
